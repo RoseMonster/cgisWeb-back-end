f190e98c6ac713a8a78378103e9c0e9031e1f857
MuZG <845417910@qq.com>
2021-01-27 17:19:36 +0800

    project init

---
+++ src/main/java/com/muzg/cgis/CgisApplication.java
@@ -0,0 +1,13 @@
+package com.muzg.cgis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CgisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/muzg/cgis/CgisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muzg.cgis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
